0c81736fa3a1e23bde9c83d2e1eaac48568801a0
salem2400 <namperone9@gmail.com>
2023-06-15 15:38:32 -0700

    last edit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
- * main - This is a description
- * Return:0 
+ *main - This is a description
+ *Return:0 
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
