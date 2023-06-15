cdf4a45f105479509c687b56901fb94612c074e9
salem2400 <namperone9@gmail.com>
2023-06-15 16:55:01 -0700

    last edit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,7 +8,7 @@ int main(void)
 	printf("Size of a char: %u byte(s)\n", sizeof(char));
 	printf("Size of an int: %u byte(s)\n", sizeof(int));
 	printf("Size of a long int: %u byte(s)\n", sizeof(long int));
-	printf("Size of a long long int:%u byte(s)\n", sizeof(long long int));
+	printf("Size of a long long int: %u byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %u byte(s)\n", sizeof(float));
 	return (0);
 }
