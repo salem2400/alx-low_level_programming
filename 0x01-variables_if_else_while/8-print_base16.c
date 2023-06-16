73c1d978d7acee8c81608ee6f35b032817e83806
salem2400 <namperone9@gmail.com>
2023-06-16 16:46:03 -0700

    last edit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - This is a description
+ * Return: 0
+ */
+int main(void){
+	int x;
+	for (x = 0 ; x < 10 ; x++)
+	{
+	putchar(x + '0');
+	}
+	int c;
+	for (c  = 97 ; c < 103 ; c++)
+	{
+	putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
