9fa19c699d2db1769218a59a506670b8a0d402db
salem2400 <namperone9@gmail.com>
2023-06-16 16:50:22 -0700

    last edit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,13 +3,15 @@
  * main - This is a description
  * Return: 0
  */
-int main(void){
+int main(void)
+{
 	int x;
+	int c;
+
 	for (x = 0 ; x < 10 ; x++)
 	{
 	putchar(x + '0');
 	}
-	int c;
 	for (c  = 97 ; c < 103 ; c++)
 	{
 	putchar(c);
