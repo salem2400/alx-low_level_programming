7a5ac19dc9a2e0e8e95472a39a0157fa7064d90c
salem2400 <namperone9@gmail.com>
2023-06-16 15:34:57 -0700

    last edit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,16 +5,16 @@
  */
 int main(void)
 {
-        int c;
+	int c;
 
-        for (c  = 97 ; c < 123 ; c++)
-        {
-        putchar(c);
-        }
+	for (c  = 97 ; c < 123 ; c++)
+	{
+	putchar(c);
+	}
 	for (c  = 65 ; c < 91 ; c++)
-        {
-        putchar(c);
-        }
-        putchar('\n');
-        return (0);
+	{
+	putchar(c);
+	}
+	putchar('\n');
+	return (0);
 }
