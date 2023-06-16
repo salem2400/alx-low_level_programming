0b3112b1c39cd9f47763c06ec4bbf0de60f5c018
salem2400 <namperone9@gmail.com>
2023-06-16 15:48:51 -0700

    last edit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - This is a description
+ * Return: 0
+ */
+int main(void)
+{
+        int c;
+
+        for (c  = 97 ; c < 123 ; c++)
+        {
+	if (c != 113 && c != 101)
+	{
+	putchar(c);
+	}
+        }
+        putchar('\n');
+        return (0);
+}
