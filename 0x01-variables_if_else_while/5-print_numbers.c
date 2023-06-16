a7b7cfb7c4af270ea97f741b5839cf7777c72f53
salem2400 <namperone9@gmail.com>
2023-06-16 15:58:25 -0700

    last edit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - This is a description
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c  = 0 ; c < 10 ; c++)
+	{
+	putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
