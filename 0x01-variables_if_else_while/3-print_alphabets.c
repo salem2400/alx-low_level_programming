606f43fcaaa4fabbb90b054302cddd035d4da8fb
salem2400 <namperone9@gmail.com>
2023-06-16 15:31:16 -0700

    last edit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - This is a description
+ * Return: 0
+ */
+int main(void)
+{
+        int c;
+
+        for (c  = 97 ; c < 123 ; c++)
+        {
+        putchar(c);
+        }
+	for (c  = 65 ; c < 90 ; c++)
+        {
+        putchar(c);
+        }
+        putchar('\n');
+        return (0);
+}
