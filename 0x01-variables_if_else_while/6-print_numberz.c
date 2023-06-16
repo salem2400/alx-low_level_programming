1068f8391d7e50d6281bc37ee374927cb52b817c
salem2400 <namperone9@gmail.com>
2023-06-16 16:05:22 -0700

    last edit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - This is a description
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0 ; c < 10 ; c++)
+	{
+	putchar(c + '0');
+	}
+	putchar('\n');
+	return (0);
+}
