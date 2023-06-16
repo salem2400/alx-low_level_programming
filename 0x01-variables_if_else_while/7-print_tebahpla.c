5477119be770eeef2341ae57e6249b27b871e260
salem2400 <namperone9@gmail.com>
2023-06-16 16:13:10 -0700

    last edit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - This is a description
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c  = 122 ; c > 96 ; c--)
+	{
+	putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
