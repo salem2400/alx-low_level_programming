fef36c8fc79937d94f12a3f67f365f030cc1d891
salem2400 <namperone9@gmail.com>
2023-06-20 05:06:54 -0700

    last edit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,18 +2,14 @@
 
 
 /**
-
  * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
-
  * followed by a new line
-
  */
 
 void print_alphabet_x10(void)
 {
 
 	char ch;
-
 	int i;
 
 	i = 0;
