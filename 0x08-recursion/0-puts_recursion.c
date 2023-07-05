a78341160024812729e2dc94b1fa87d9aba72596
salem2400 <namperone9@gmail.com>
2023-07-05 22:39:56 +0000

    last edit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,7 +4,7 @@
  * main - check the code
  * Return: Always 0.
  */
-void_puts_recursion(char *s)
+void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
@@ -12,5 +12,5 @@ void_puts_recursion(char *s)
 		return (0);
 	}
 	_putchar(*s);
-	_putchar(s + 1);
+	_putchar(*s + 1);
 }
