0d93e8f3cb85d4f04ac508cd47f74b728411cf9d
salem2400 <namperone9@gmail.com>
2023-07-05 23:14:14 +0000

    last edit

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *  _print_rev_recursion- print a string in revers.
+ *  @s: point to the first char in string.
+ *  Return 0 - always
+ */
+
+void  _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar (*s);
+		_print_rev_recursion(s + 1);
+
+	}
+	else
+	{
+		putchar('\n');
+		return;
+	}
+}
+
