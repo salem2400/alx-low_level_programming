dcae497620d712e110f633689f3d71a0d2062b4e
salem2400 <namperone9@gmail.com>
2023-07-05 22:29:10 +0000

    last edit

--- 0x08-recursion/_putchar.c
+++ 0x08-recursion/_putchar.c
@@ -1,12 +1,16 @@
 #include "main.h"
-#include <nuistd.h>
+#include <unistd.h>
+
 /**
- * _putchar-writes the character c to stdout
+ * _putchar - writes the character c to stdout
  * @c: The character to print
- * Return: on success 1.
- * on error, -1 is returned, and errno is set appropriatly.
- *\
- int _putchar(char c)
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _putchar(char c)
+
 {
-	return (write(1,&c,1));
+	return (write(1, &c, 1));
 }
