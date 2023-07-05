1580f573f7ad67c4674a1c5a2493d7fcde23cdb6
salem2400 <namperone9@gmail.com>
2023-07-05 22:35:18 +0000

    last edit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,10 +4,8 @@
  * main - check the code
  * Return: Always 0.
  */
-int main(void)
-{
-	_puts_recursion("Puts with recursion");
-	if (s == '\0')
+void_puts_recursion(char *s);
+	if (*s == '\0')
 	{
 		_putchar('\n');
 		return (0);
