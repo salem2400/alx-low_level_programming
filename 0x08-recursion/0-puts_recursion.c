be5be765a6928e21c22b5114c15c95acfb668545
salem2400 <namperone9@gmail.com>
2023-07-05 21:46:40 +0000

    last edit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *var = "string";
+
+	printf("%s\n", var);
+	return (0);
+}
---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <nuistd.h>
+/**
+ * _putchar-writes the character c to stdout
+ * @c: The character to print
+ * Return: on success 1.
+ * on error, -1 is returned, and errno is set appropriatly.
+ *\
+ int _putchar(char c)
+{
+	return (write(1,&c,1));
+}
