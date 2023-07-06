24c8fbe8880ce1ab078392684052eb2c07902f07
salem2400 <namperone9@gmail.com>
2023-07-06 09:19:47 +0000

    last edit

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -11,12 +11,7 @@
 
 int _strlen_recursion(char *s)
 {
-
-	if (strlen(s) == '\0')
-	{
-		_strlen_recursion("Corbin Coleman");
-		return (strlen(s));
-	}
-	return (0);
-	}
-
+	if (*s == '\0')
+	return 0;
+	return 1 + _strlen_recursion(s + 1);
+}
