6729cba905f13eafb25e8142a67dc99beabc40d4
salem2400 <namperone9@gmail.com>
2023-07-07 15:04:09 +0000

    g

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,7 +6,8 @@
 	#include <stdio.h>
 
 /**
- * main - Lists all the natural numbers below 1024 (excluded)that are multiples of 3 or 5.
+ * main - Lists all the natural numbers below 1024 (excluded)
+ * that are multiples of 3 or 5.
  * Return: Always 0.
  */
 	int main(void)
--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,7 +2,8 @@
 
 
 /**
- * main - Prints first 50 Fibonacci numbers, starting with 1 and 2, separated by a comma followed by a spaceeet nu
+ * main - Prints first 50 Fibonacci numbers, starting with 1 and 2,
+ * separated by a comma followed by a spaceeet nu
  et
  * Return: Always 0.
  */
