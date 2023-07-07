80906ef59ebf7a70ad65dd63562c87648a8b4388
salem2400 <namperone9@gmail.com>
2023-07-07 18:59:57 +0000

    sa

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* main - tests function that prints if integer is positive or negative
+* main - Test function for positive or negative
 * Return: 0
 */
 
@@ -9,8 +9,8 @@ int main(void)
 {
 	int i;
 
-	i = 98 % 2;
+	i = 0;
 	positive_or_negative(i);
-
 	return (0);
+
 }
