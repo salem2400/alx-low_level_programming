cb614a99f81e7a4c39003b7e02821ae422b0118b
salem2400 <namperone9@gmail.com>
2023-07-13 18:25:50 +0000

    last edit

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,13 +1,27 @@
 #include "main.h"
 #include <stdlib.h>
+#include <string.h>
 char *_strdup(char *str)
 {
 	char *cpy;
 
 	cpy = _strdup (str);
-		if (cpy == NULL)
+	cpy = malloc(sizeof(char *));
+	if (cpy == NULL)
+	{                         
+		return (NULL);
+	}
+	while (strcmp(str, cpy) != 0)
+	{
+		int i;
+
+		i = 0;
+		i++;
+		cpy [i] = str [i];
+		if (strcmp(str, cpy) == 0)
 		{
 			return (NULL);
 		}
-	return (cpy);
+	}
+return (cpy);
 }
