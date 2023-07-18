bec924d4a381b03ad95d3bdde0b32280f1b24cd9
salem2400 <namperone9@gmail.com>
2023-07-18 19:12:29 +0000

    last edit

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ *
+ *
+ */
+void print_dog(struct dog *d)
+{
+	d = malloc(sizeof(struct dog));
+	if (d == NULL)
+
+		return;
+	printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
+
+	printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
+
+	free (d);
+	return;
+}
