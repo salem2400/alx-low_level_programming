a96aaac86e7147ccc604220a7612d605cf56e4a2
salem2400 <namperone9@gmail.com>
2023-07-27 20:31:43 +0000

    last edit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,14 +9,13 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+        size_t count = 0;
 
-	while (h != NULL)
-	{
-		printf("[%d] %s\n", h->len, h->str != NULL ? h->str : "(nil)");
-		h = h->next;
-		count++;
-	}
-	return (count);
+        while (h != NULL)
+        {
+                printf("[%d] %s\n", h->len, h->str != NULL ? h->str : "(nil)");
+                h = h->next;
+                count++;
+        }
+        return (count);
 }
-
