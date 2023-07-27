b0ef44c6f3727fbf3774896b8dce50924b0b345f
salem2400 <namperone9@gmail.com>
2023-07-27 16:34:22 +0000

    last edit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,20 +2,20 @@
 
 /**
  * print_list - orint the list elements
- * @head: have the elements
+ * @h: have the elements
  * Return: the value of y
  */
-size_t print_list(const list_t *head)
+size_t print_list(const list_t *h)
 {
 	size_t s = 0;
 
-	while (head)
+	while (h)
 	{
-		if (!head->str)
+		if (!h->str)
 			printf("[0] (nil)\n");
 		else
-			printf("[%u] %s\n", head->len, head->str);
-		head = head->next;
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
 		s++;
 	}
 
