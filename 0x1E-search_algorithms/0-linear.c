6d08e0bbfb7b3bc47d18ff766b8cd90bd292f073
salem2400 <namperone9@gmail.com>
2024-03-11 16:10:50 +0200

    hi, i'm in bin

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,16 +1,19 @@
-#include <stdio.h>
+#include "search_algos.h"
 
-int linear_search(int *array, size_t size, int value) {
-    if (array == NULL) {
-        return -1;
-    }
+int linear_search(int *array, size_t size, int value)
+{
+	int x;
 
-    for (size_t i = 0; i < size; i++) {
-        printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-        if (array[i] == value) {
-            return i;
-        }
-    }
-
-    return -1;
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	for (x = 0; x < size; x++)
+	{
+		if (value == array[x])
+		{
+			return (x);
+		}
+	}
+	return (-1);
 }
